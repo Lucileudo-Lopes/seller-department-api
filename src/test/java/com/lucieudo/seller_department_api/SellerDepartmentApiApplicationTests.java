0d148c907968ed44d42f859f3f98921f1e9f8b19
Lucileudo Paiva <lucileudodepaivalopes@gmail.com>
2026-07-28 00:01:12 -0300

    config: initial Spring Boot project setup with dependencies

---
+++ src/test/java/com/lucieudo/seller_department_api/SellerDepartmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucieudo.seller_department_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerDepartmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
