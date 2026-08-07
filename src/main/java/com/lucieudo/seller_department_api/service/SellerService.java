f52a8c16ccf8e7207d2061f1cf0e0b55eb0f9b7c
Lucileudo Paiva <lucileudodepaivalopes@gmail.com>
2026-08-07 17:40:17 -0300

    feat: add SellerService with CRUD operations

---
+++ src/main/java/com/lucieudo/seller_department_api/service/SellerService.java
@@ -0,0 +1,33 @@
+package com.lucieudo.seller_department_api.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucieudo.seller_department_api.entity.Seller;
+import com.lucieudo.seller_department_api.repository.SellerRepository;
+
+@Service
+public class SellerService {
+
+	@Autowired
+	private SellerRepository repository;
+
+	public List<Seller> findAll() {
+		return repository.findAll();
+	}
+
+	public Seller findById(Integer id) {
+		return repository.findById(id).orElseThrow(() -> new RuntimeException("Seller not found with id: " + id));
+
+	}
+
+	public Seller save(Seller obj) {
+		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		repository.deleteById(id);
+	}
+}
