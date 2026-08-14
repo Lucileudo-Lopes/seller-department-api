303e674280921de79a93a21fd7d5c10289134c6a
Lucileudo Paiva <lucileudodepaivalopes@gmail.com>
2026-08-13 21:42:18 -0300

    feat: implement global exception handling

---
+++ src/main/java/com/lucieudo/seller_department_api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.lucieudo.seller_department_api.exception;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), "Resouce not found", e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<StandardError> gereralException(Exception e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+		StandardError err = new StandardError(Instant.now(), status.value(), "Internal server error", e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+
+	}
+}
---
+++ src/main/java/com/lucieudo/seller_department_api/exception/StandardError.java
@@ -0,0 +1,23 @@
+package com.lucieudo.seller_department_api.exception;
+
+import java.time.Instant;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StandardError {
+
+	private Instant timestamp;
+
+	private Integer status;
+
+	private String error;
+
+	private String message;
+
+	private String path;
+}
